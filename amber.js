93923dd88be224e40abd9655c51a8baaa97e23de
cjhuang <cjhuang@wireless-10-105-98-180.umd.edu>
2019-12-12 17:33:56 -0500

    Text function added

--- amber.js
+++ amber.js
@@ -53,8 +53,8 @@ d3.csv("counties-missing-child.csv", function(data) {
                 data[i].position = dataValue[0][0][0];
               }
               else{
-                var varX = d3.randomUniform(dataValue[0][0][0], dataValue[0][2][0])();
-                var varY = d3.randomUniform(dataValue[0][0][1], dataValue[0][2][1])();
+                var varX = d3.randomUniform(dataValue[0][0][0], dataValue[0][3][0])();
+                var varY = d3.randomUniform(dataValue[0][0][1], dataValue[0][3][1])();
                 data[i].position = [varX, varY];
               }
               data[i].lostLocation = jsonCounty;
@@ -95,23 +95,29 @@ d3.csv("counties-missing-child.csv", function(data) {
             setTimeout(function(){
             d3.select("#lostYearNumber").text('58');
             d3.select("#lostChildNumber").text('1');
-            circleDraw(0);
-            orangeLineDraw(0);
-            whiteLineDraw(0);
+            // circleDraw(0);
+            // orangeLineDraw(0);
+            // whiteLineDraw(0);
+            animateDraw(0);
+            labelWrite1(0);
+            labelWrite2(0);
             if(typeof callback == 'function')
             callback();}, 1000);
           }
-          
+
           function secondAnimation(callback){
             setTimeout(function(){
             setTimeout(function(){
             d3.select("#lostYearNumber").text('86');
             d3.select("#lostChildNumber").text('2');
-            circleDraw(1);
-            orangeLineDraw(1);
-            whiteLineDraw(1);}, 3000);
+            // circleDraw(1);
+            // orangeLineDraw(1);
+            // whiteLineDraw(1);}
+            animateDraw(1);
+            labelWrite1(1);
+            labelWrite2(1);}, 2000);
             if(typeof callback == 'function')
-            callback();}, 0);
+            callback();}, 2000);
           }
           
           var lostKidNumber = 2;
@@ -121,7 +127,7 @@ d3.csv("counties-missing-child.csv", function(data) {
             setTimeout(function(){
               for (var i = 3; i < newData.length; i++) {
                 meteourEffect(i);}
-            }, 5000);
+            }, 3000);
           }
 
           function meteourEffect(i){ 
@@ -132,14 +138,76 @@ d3.csv("counties-missing-child.csv", function(data) {
               var eachLostYear = 2020 - parseInt(newData[i].BirthDay.slice(-4)) - parseInt(newData[i].Age);
               lostYear += eachLostYear;}
               d3.select("#lostYearNumber").text(lostYear);
-              circleDraw(i);
-              orangeLineDraw(i);
-              whiteLineDraw(i);
+              animateDraw(i);
+              //labelWrite1(i);
+              // circleDraw(i);
+              // orangeLineDraw(i);
+              // whiteLineDraw(i);
             }, 5*i);
           }
 
+          // function executeAsynchronously(functions, timeout) {
+          //   for(var i = 0; i < functions.length; i++) {
+          //     setTimeout(functions[i], timeout);
+          //   }
+          // }
+
+          // function animateDraw(label){
+          // executeAsynchronously(
+          //   [circleDraw(label), orangeLineDraw(label), whiteLineDraw(label)], 10000);
+          // }
+
+          //Intergrate Animation
+          function animateDraw(label){
+            // circleDraw(label);
+            // orangeLineDraw(label);
+            // whiteLineDraw(label);
+            circleDraw(label, function() {
+              orangeLineDraw(label, function() {
+                whiteLineDraw(label, function() {
+                  //All three functions have completed, in order.
+                });
+              });
+            });
+          }
+
+          //Function for writing the first label to describe when does the kid lose
+          function labelWrite1(label){
+            var locationX = projection([newData[label].position[0], newData[label].position[1]])[0];
+            var locationY = projection([newData[label].position[0], newData[label].position[1]])[1];
+            
+            svg.append("text")
+              .attr("x", locationX + 10)
+              .attr("y", locationY + 30)
+              .attr("font-family", "roboto")
+              .attr("font-size", 14)
+              .attr("fill", "#D8D8D8")
+              .text(newData[label].Name + ", missed at " + newData[label].Age + ".");
+            
+            //svg.select("#shit").remove()
+            //callback();
+          }
+
+          //Function for writing the second label to describe how old the kid would be if he/she survives
+          function labelWrite2(label){
+            var locationX = projection([newData[label].position[0], newData[label].position[1]])[0];
+            var locationY = projection([newData[label].position[0], newData[label].position[1]])[1];
+            
+            svg.append("text")
+              .attr("x", locationX + 8)
+              .attr("y", locationY + 17)
+              .attr("font-family", "roboto")
+              .attr("font-size", 14)
+              .attr("font-style", "italic")
+              .attr("fill", "#D8D8D8")
+              .text("Could have been " + (2020 - parseInt(newData[label].BirthDay.slice(-4)) + 1) + " now");
+            
+              //svg.select("#shit").remove()
+            //callback();
+          }
+          
           //Function for drawing circle, 2 different lines
-          function circleDraw(label){
+          function circleDraw(label, callback){
             var locationX = projection([newData[label].position[0], newData[label].position[1]])[0];
             var locationY = projection([newData[label].position[0], newData[label].position[1]])[1];
             var eachLostYear = 2020 - parseInt(newData[label].BirthDay.slice(-4)) - parseInt(newData[label].Age);
@@ -152,9 +220,11 @@ d3.csv("counties-missing-child.csv", function(data) {
               //.duration(100)
               .ease('linear')
               .style("fill", "#FF4112");
+
+            callback();
           }
 
-          function orangeLineDraw(label){
+          function orangeLineDraw(label, callback){
             if(newData[label].Age.length != 0){
             var locationX = projection([newData[label].position[0], newData[label].position[1]])[0];
             var locationY = projection([newData[label].position[0], newData[label].position[1]])[1];
@@ -172,9 +242,10 @@ d3.csv("counties-missing-child.csv", function(data) {
             .attr({x2: locationX - 3*parseInt(newData[label].Age)*angleCal(newData[label].locationCount)[0],
                    y2: locationY + 3*parseInt(newData[label].Age)*angleCal(newData[label].locationCount)[1]});
                   }
+            callback();
                 }
 
-          function whiteLineDraw(label){
+          function whiteLineDraw(label, callback){
             if(newData[label].Age.length != 0){
             var locationX = projection([newData[label].position[0], newData[label].position[1]])[0];
             var locationY = projection([newData[label].position[0], newData[label].position[1]])[1];
@@ -193,6 +264,7 @@ d3.csv("counties-missing-child.csv", function(data) {
              .attr({x2: locationX - 3*parseInt(newData[label].Age)*angleCal(newData[label].locationCount)[0] - 3*eachLostYear*angleCal(newData[label].locationCount)[0],
                     y2: locationY + 3*parseInt(newData[label].Age)*angleCal(newData[label].locationCount)[1] + 3*eachLostYear*angleCal(newData[label].locationCount)[1]}); 
                   }
+            callback();
                 }
 
           //Hover
